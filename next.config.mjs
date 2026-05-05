9e98ecc9b58e6a9ffaac2f011bf9ca7754cb71b2
FABIANO SANNINO <46810617+fsannino@users.noreply.github.com>
2026-05-05 03:20:42 -0300

    chore: move pmo-modern/* para a raiz do repo (simplifica deploy)

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: process.env.DOCKER_BUILD === "1" ? "standalone" : undefined,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "20mb",
+    },
+  },
+};
+
+export default nextConfig;
